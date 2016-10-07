58b2eec2cf1dc3f9ec1e7e41e5b4eb1bab8fa993
Chris Brown (OhBlihv) <dev@ohblihv.me>
2016-10-07 16:31:18 +0800

    Improve Packet ActionBar/Title Persistence

--- src/main/java/com/skytonia/SkyCore/util/PacketUtil.java
+++ src/main/java/com/skytonia/SkyCore/util/PacketUtil.java
@@ -9,31 +9,117 @@ import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * Created by Chris Brown (OhBlihv) on 2/10/2016.
  */
 public class PacketUtil
 {
 	
+	public enum PersistingType
+	{
+		
+		ACTION_BAR,
+		TITLE;
+		
+	}
+	
+	public static class PersistingRunnable
+	{
+		
+		private int taskId;
+		private int executionCount = 0,
+					maxExecutions;
+		
+		public PersistingRunnable(long tickDelay, int executions, Runnable runnable)
+		{
+			//No executions necessary.
+			if(executions <= 0)
+			{
+				return;
+			}
+			
+			this.maxExecutions = executions;
+			
+			//Run immediately, then every 'tickDelay' ticks.
+			this.taskId = Bukkit.getScheduler().runTaskTimer(SkyCore.getInstance(), () ->
+			{
+				try
+				{
+					runnable.run();
+					
+					if(++executionCount == maxExecutions)
+					{
+						cancelRunnable();
+					}
+				}
+				//Just in case.
+				catch(Exception e)
+				{
+					cancelRunnable();
+				}
+			}, 1, tickDelay).getTaskId();
+		}
+		
+		public void cancelRunnable()
+		{
+			if(taskId != -1)
+			{
+				Bukkit.getScheduler().cancelTask(taskId);
+				taskId = -1;
+			}
+		}
+		
+	}
+	
+	public static class Persistence
+	{
+		
+		private final Map<PersistingType, PersistingRunnable> persistingRunnables = new EnumMap<>(PersistingType.class);
+		
+		public void addOrReplaceRunnable(PersistingType persistingType, long tickDelay, int executions, Runnable runnable)
+		{
+			if(persistingRunnables.containsKey(persistingType))
+			{
+				persistingRunnables.get(persistingType).cancelRunnable();
+			}
+			
+			persistingRunnables.put(persistingType, new PersistingRunnable(tickDelay, executions, runnable));
+		}
+		
+	}
+	
+	private static Map<UUID, Persistence> persistenceMap = new HashMap<>();
+	
 	public static void sendActionBar(Player player, String message, int lifespan)
 	{
 		PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
 		PacketPlayOutChat packetPlayOutChat = new PacketPlayOutChat(new ChatComponentText(message), (byte) 2);
-		
-		sendPacket(playerConnection, packetPlayOutChat);
-		//Since we're updating every 2 seconds, we only need to send half as many updates
-		for(int i = 1;i < ((lifespan / 2) + 1);i++)
-		{
-			Bukkit.getScheduler().runTaskLater(SkyCore.getInstance(), () ->
-			{
-				sendPacket(playerConnection, packetPlayOutChat);
-			}, 40L * i); //Action bar lasts about 2-3 seconds. Send an update after 2 seconds to ensure it does not disappear
-		}
+
+		//Action bar lasts about 2-3 seconds. Send an update after 2 seconds to ensure it does not disappear
+		startPersistingTask(player.getUniqueId(), PersistingType.ACTION_BAR, 40L, lifespan / 2,
+		                    () -> sendPacket(playerConnection, packetPlayOutChat));
 	}
 	
 	private static void sendPacket(PlayerConnection playerConnection, Packet packet)
 	{
 		playerConnection.sendPacket(packet);
+	}
+	
+	public static void startPersistingTask(UUID player, PersistingType persistingType, long tickDelay, int executions, Runnable runnable)
+	{
+		Persistence playerPersistence;
+		if((playerPersistence = persistenceMap.get(player)) == null)
+		{
+			playerPersistence = new Persistence();
+			persistenceMap.put(player, playerPersistence);
+		}
+		
+		playerPersistence.addOrReplaceRunnable(persistingType, tickDelay, executions, runnable);
 	}
 	
 }
