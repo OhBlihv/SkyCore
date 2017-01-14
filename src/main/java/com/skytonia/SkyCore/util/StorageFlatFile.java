58f0db07171f55b0caeae6b2c371a8f1067d0c53
Chris Brown <dev@ohblihv.me>
2017-01-14 12:25:25 +0800

    Add Storage FlatFile skeleton class

---
+++ src/main/java/com/skytonia/SkyCore/util/StorageFlatFile.java
@@ -0,0 +1,30 @@
+package com.skytonia.SkyCore.util;
+
+import org.bukkit.Bukkit;
+
+/**
+ * Created by Chris Brown (OhBlihv) on 12/28/2016.
+ */
+public abstract class StorageFlatFile extends FlatFile
+{
+	
+	protected StorageFlatFile(String fileName, String owningPlugin, long saveTicks)
+	{
+		super(fileName, owningPlugin);
+		
+		load();
+		
+		if(saveTicks <= 300L)
+		{
+			BUtil.logInfo("(" + fileName + ") given saveTicks below 15 seconds '" + saveTicks + " ticks'");
+			saveTicks = 300L;
+		}
+		
+		Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, this::save, saveTicks, saveTicks);
+	}
+	
+	public abstract void save();
+	
+	public abstract void load();
+	
+}
