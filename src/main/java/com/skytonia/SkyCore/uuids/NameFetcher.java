bbf24b6ff4817cba465102fa79faabede7274d06
Chris Brown <dev@ohblihv.me>
2017-02-25 15:29:09 +0800

    Add NameFetcher/UUIDFetcher as a last-resort after SkyPerms

---
+++ src/main/java/com/skytonia/SkyCore/uuids/NameFetcher.java
@@ -0,0 +1,132 @@
+package com.skytonia.SkyCore.uuids;
+
+import com.google.common.collect.ImmutableList;
+import com.skytonia.SkyCore.util.BUtil;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+
+/**
+ * This class is used to get the name of a player from a UUID.
+ * <p>
+ * Date created: 17:02:13 2 apr. 2014
+ *
+ * @author evilmidget38
+ */
+public class NameFetcher implements Callable<Map<UUID, String>>
+{
+	
+	private static final String PROFILE_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
+	
+	public static String fromStream(final InputStream in) throws IOException
+	{
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		final StringBuilder out = new StringBuilder();
+		final String newLine = System.getProperty("line.separator");
+		String line;
+		
+		while((line = reader.readLine()) != null)
+		{
+			out.append(line);
+			out.append(newLine);
+		}
+		return out.toString();
+	}
+	
+	private final JSONParser jsonParser = new JSONParser();
+	
+	private final List<UUID> uuids;
+	
+	public NameFetcher(final List<UUID> uuids)
+	{
+		this.uuids = ImmutableList.copyOf(uuids);
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.util.concurrent.Callable#call()
+	 */
+	@Override
+	public Map<UUID, String> call() throws Exception
+	{
+		final Map<UUID, String> uuidStringMap = new HashMap<>();
+		for(final UUID uuid : uuids)
+		{
+			final HttpURLConnection connection = (HttpURLConnection)
+				                                  new URL(PROFILE_URL + uuid.toString().replace("-", "")).openConnection();
+			
+			JSONObject response;
+			String name = null;
+			String fromStream;
+			// Ping code 204 == No content (Request was sent, but UUID was invalid)
+			//final int pingCode = connection.getResponseCode();
+
+			/*if (pingCode == 204) {
+				Bukkit.getLogger().warning("Tried to get UUID: " + uuid.toString() + " but this invalid.");
+				continue;
+			}*/
+
+			/*if (pingCode == 204) {
+				System.out.print("Got 204 code - no content.");
+				continue;
+			}*/
+			
+			//System.out.print("Ping: " + pingCode);
+			
+			try
+			{
+				response = (JSONObject) jsonParser.parse(new InputStreamReader(
+					                                                              connection.getInputStream()));
+				
+				name = (String) response.get("name");
+				
+			}
+			catch(final ParseException e)
+			{
+				// Try converting the stream to a string and removing all the spaces. 
+				fromStream = fromStream(connection.getInputStream())
+					             .replaceAll(" ", "");
+				
+				// Parse again
+				response = (JSONObject) jsonParser.parse(fromStream);
+				
+				// Should work now!
+				name = (String) response.get("name");
+				
+				if(name == null)
+				{
+					BUtil.logError("Could not parse uuid '" + uuid.toString() + "' to name!");
+					continue;
+				}
+				
+				final String cause = (String) response.get("cause");
+				final String errorMessage = (String) response.get("errorMessage");
+				if(cause != null && !cause.isEmpty())
+				{
+					throw new IllegalStateException(errorMessage);
+				}
+			}
+			finally
+			{
+				if(name == null)
+				{
+					BUtil.logInfo("Could not find name of account with uuid: '"	+ uuid.toString() + "'");
+				}
+			}
+			
+			uuidStringMap.put(uuid, name);
+		}
+		return uuidStringMap;
+	}
+}
---
+++ src/main/java/com/skytonia/SkyCore/uuids/UUIDFetcher.java
@@ -0,0 +1,121 @@
+package com.skytonia.SkyCore.uuids;
+
+import com.google.common.collect.ImmutableList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+
+/**
+ * This class is used to get the UUID of a certain player.
+ * <p>
+ * Date created: 17:00:41 2 apr. 2014
+ *
+ * @author evilmidget38
+ */
+public class UUIDFetcher implements Callable<Map<String, UUID>>
+{
+	
+	private static final String PROFILE_URL = "https://api.mojang.com/profiles/minecraft";
+	private static final double PROFILES_PER_REQUEST = 100;
+	
+	private static HttpURLConnection createConnection() throws Exception
+	{
+		final URL url = new URL(PROFILE_URL);
+		final HttpURLConnection connection = (HttpURLConnection) url
+			                                                         .openConnection();
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("Content-Type", "application/json");
+		connection.setUseCaches(false);
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+		return connection;
+	}
+	
+	private static UUID getUUID(final String id)
+	{
+		return UUID.fromString(id.substring(0, 8) + "-" + id.substring(8, 12)
+			                       + "-" + id.substring(12, 16) + "-" + id.substring(16, 20) + "-"
+			                       + id.substring(20, 32));
+	}
+	
+	public static UUID getUUIDOf(final String name) throws Exception
+	{
+		return new UUIDFetcher(Arrays.asList(name)).call().get(name);
+	}
+	
+	private static void writeBody(final HttpURLConnection connection,
+	                              final String body) throws Exception
+	{
+		final OutputStream stream = connection.getOutputStream();
+		stream.write(body.getBytes());
+		stream.flush();
+		stream.close();
+	}
+	
+	private final JSONParser jsonParser = new JSONParser();
+	
+	private final List<String> names;
+	
+	private final boolean rateLimiting;
+	
+	public UUIDFetcher(final List<String> names)
+	{
+		this(names, true);
+	}
+	
+	public UUIDFetcher(final List<String> names, final boolean rateLimiting)
+	{
+		this.names = ImmutableList.copyOf(names);
+		this.rateLimiting = rateLimiting;
+	}
+	
+	@Override
+	public Map<String, UUID> call() throws Exception
+	{
+		final Map<String, UUID> uuidMap = new HashMap<>();
+		final int requests = (int) Math.ceil(names.size() / PROFILES_PER_REQUEST);
+		for(int i = 0; i < requests; i++)
+		{
+			final HttpURLConnection connection = createConnection();
+			final String body = JSONArray.toJSONString(names.subList(i * 100,
+			                                                         Math.min((i + 1) * 100, names.size())));
+			writeBody(connection, body);
+			JSONArray array;
+			
+			//try {
+			array = (JSONArray) jsonParser.parse(new InputStreamReader(connection.getInputStream()));
+			/*} catch (final Exception e) {
+				util.logError("Could not fetch UUID of player '"
+						+ names.get(i) + "'!");
+				continue;
+			}*/
+			
+			for(final Object profile : array)
+			{
+				final JSONObject jsonProfile = (JSONObject) profile;
+				final String id = (String) jsonProfile.get("id");
+				final String name = (String) jsonProfile.get("name");
+				
+				final UUID uuid = UUIDFetcher.getUUID(id);
+				
+				uuidMap.put(name, uuid);
+			}
+			if(rateLimiting && i != requests - 1)
+			{
+				Thread.sleep(100L);
+			}
+		}
+		return uuidMap;
+	}
+}
