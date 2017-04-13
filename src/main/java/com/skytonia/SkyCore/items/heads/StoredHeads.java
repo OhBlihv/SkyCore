dfcdba639bfe1064a6ad5af59c94674955d27909
OhBlihv <dev@ohblihv.me>
2017-04-13 17:29:32 +0800

    Properly set item properties when creating stored head items

--- src/main/java/com/skytonia/SkyCore/items/heads/StoredHeads.java
+++ src/main/java/com/skytonia/SkyCore/items/heads/StoredHeads.java
@@ -4,6 +4,7 @@ import com.skytonia.SkyCore.items.HeadUtil;
 import com.skytonia.SkyCore.items.construction.ItemContainer;
 import com.skytonia.SkyCore.items.construction.ItemContainerConstructor;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
 
@@ -23,7 +24,10 @@ public class StoredHeads
 		
 		public ItemContainer asItemContainer()
 		{
-			return new ItemContainerConstructor.ItemContainerBuilder().skullTexture(skullTexture).build();
+			return new ItemContainerConstructor.ItemContainerBuilder()
+				       .material(Material.SKULL_ITEM)
+					   .damage(3)
+				       .skullTexture(skullTexture).build();
 		}
 		
 		public ItemStack buildHead()
