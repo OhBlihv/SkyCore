afbe56fd15d13cf5c1d341fd9fcb4be5405da367
OhBlihv <dev@ohblihv.me>
2017-04-25 14:48:21 +0800

    Move Pair Helper Class to SkyCore

---
+++ src/main/java/com/skytonia/SkyCore/util/Pair.java
@@ -0,0 +1,16 @@
+package com.skytonia.SkyCore.util;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by Chris Brown (OhBlihv) on 4/18/2017.
+ */
+@RequiredArgsConstructor
+public class Pair<A, B>
+{
+	
+	public final A left;
+	
+	public final B right;
+	
+}
