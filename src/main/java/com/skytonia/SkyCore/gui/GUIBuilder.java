1c92350e0cfa8317aea1d8132f853146da6a291b
OhBlihv <dev@ohblihv.me>
2017-06-09 15:47:43 +0800

    Increase stability of GUIBuilder

--- src/main/java/com/skytonia/SkyCore/gui/GUIBuilder.java
+++ src/main/java/com/skytonia/SkyCore/gui/GUIBuilder.java
@@ -58,104 +58,114 @@ public class GUIBuilder<T>
 	
 	public T build(ConfigurationSection configurationSection)
 	{
-		guiTitle = BUtil.translateColours(configurationSection.getString("title", "Inventory"));
-		
 		InventorySize inventorySize;
-		{
-			//Legacy Support for number sizes
-			if(configurationSection.isInt("size"))
-			{
-				inventorySize = InventorySize.ofSize(configurationSection.getInt("size", 54));
-			}
-			else
-			{
-				try
-				{
-					inventorySize = InventorySize.valueOf(configurationSection.getString("size"));
-				}
-				//Catch Everything we can
-				catch(Exception e)
-				{
-					String inventoryString;
-					try
-					{
-						inventoryString = configurationSection.getString("size");
-					}
-					catch(NullPointerException e2)
-					{
-						inventoryString = "null";
-					}
-					catch(Exception e2)
-					{
-						inventoryString = ">unknown<";
-						e.printStackTrace();
-					}
-					
-					BUtil.logInfo("Unknown Inventory Size '" + inventoryString + "'. Defaulting to SIX_LINE");
-					BUtil.logInfo("Options: " + Arrays.toString(InventorySize.values()) + " (Or a slot amount, eg '18')");
-					inventorySize = InventorySize.SIX_LINE;
-				}
-			}
-		}
 		
-		String 	requiredPermission = configurationSection.getString("permission", null),
-			noPermissionMessage = BUtil.translateColours(configurationSection.getString("no-permission", ""));
+		String 	requiredPermission,
+			noPermissionMessage;
 		
 		GUISound openSound = null;
-		if(configurationSection.contains("open-sound") && configurationSection.isConfigurationSection("open-sound"))
-		{
-			ConfigurationSection soundSection = configurationSection.getConfigurationSection("open-sound");
-			Sound sound = null;
-			try
-			{
-				sound = Sound.valueOf(soundSection.getString("sound"));
-			}
-			catch(IllegalArgumentException e)
-			{
-				//Check if it's ITEM_PICKUP and translate
-				if(soundSection.getString("sound").equals("ITEM_PICKUP"))
-				{
-					sound = Sound.ENTITY_ITEM_PICKUP;
-				}
-			}
-			catch(NullPointerException e)
-			{
-				//Sound stays null and is caught by the next check
-			}
-			
-			if(sound == null)
-			{
-				BUtil.logInfo("Could not load sound '" + (soundSection.contains("sound") ? soundSection.getString("sound", "none") : "null") + "'");
-			}
-			else
-			{
-				openSound = new GUISound(sound, (float) soundSection.getDouble("volume", 10F), (float) soundSection.getDouble("pitch", 1F));
-			}
-		}
 		
 		ItemStack fillerItem = null;
-		if(configurationSection.contains("filler"))
+		
+		Deque<GUIElementInfo> guiElements;
+		Deque<GUIVariable> guiVariables;
+		
+		if(configurationSection == null)
 		{
-			fillerItem = ItemContainerConstructor.buildItemContainer(configurationSection.getConfigurationSection("filler")).toItemStack();
+			guiTitle = "Inventory";
+			inventorySize = InventorySize.SIX_LINE;
+			
+			requiredPermission = null;
+			noPermissionMessage = null;
+			
+			guiElements = new ArrayDeque<>();
+			guiVariables = new ArrayDeque<>();
 		}
-		/*else
+		else
 		{
-			//If we're SkyCore and not another plugin, use a default filler item based on config
-			//Plugin pluginInstance = SkyCore.getPluginInstance();
-			/*if(pluginInstance != null && pluginInstance.getClass().getSimpleName().equals("SkyCore"))
+			guiTitle = BUtil.translateColours(configurationSection.getString("title", "Inventory"));
+			
 			{
-				fillerItem = new ItemContainerBuilder()
-					             .material(Material.STAINED_GLASS_PANE)
-					             .damage(7)
-					             .displayname("§8" + FlatFile.getInstance().getString("options.server-name"))
-					             .build().toItemStack();
-			}*/
-		//}*/
-		
-		Deque<GUIElementInfo> guiElements = loadGUIElements(configurationSection, inventorySize.getSize());
-		
-		Deque<GUIVariable> guiVariables = new ArrayDeque<>();
-		guiVariables.addAll(GUIVariables.getInstance().getAllRegisteredVariables());
+				//Legacy Support for number sizes
+				if(configurationSection.isInt("size"))
+				{
+					inventorySize = InventorySize.ofSize(configurationSection.getInt("size", 54));
+				}
+				else
+				{
+					try
+					{
+						inventorySize = InventorySize.valueOf(configurationSection.getString("size"));
+					}
+					//Catch Everything we can
+					catch(Exception e)
+					{
+						String inventoryString;
+						try
+						{
+							inventoryString = configurationSection.getString("size");
+						}
+						catch(NullPointerException e2)
+						{
+							inventoryString = "null";
+						}
+						catch(Exception e2)
+						{
+							inventoryString = ">unknown<";
+							e.printStackTrace();
+						}
+						
+						BUtil.logInfo("Unknown Inventory Size '" + inventoryString + "'. Defaulting to SIX_LINE");
+						BUtil.logInfo("Options: " + Arrays.toString(InventorySize.values()) + " (Or a slot amount, eg '18')");
+						inventorySize = InventorySize.SIX_LINE;
+					}
+				}
+			}
+			
+			requiredPermission = configurationSection.getString("permission", null);
+			noPermissionMessage = BUtil.translateColours(configurationSection.getString("no-permission", ""));
+			
+			if(configurationSection.contains("open-sound") && configurationSection.isConfigurationSection("open-sound"))
+			{
+				ConfigurationSection soundSection = configurationSection.getConfigurationSection("open-sound");
+				Sound sound = null;
+				try
+				{
+					sound = Sound.valueOf(soundSection.getString("sound"));
+				}
+				catch(IllegalArgumentException e)
+				{
+					//Check if it's ITEM_PICKUP and translate
+					if(soundSection.getString("sound").equals("ITEM_PICKUP"))
+					{
+						sound = Sound.ENTITY_ITEM_PICKUP;
+					}
+				}
+				catch(NullPointerException e)
+				{
+					//Sound stays null and is caught by the next check
+				}
+				
+				if(sound == null)
+				{
+					BUtil.logInfo("Could not load sound '" + (soundSection.contains("sound") ? soundSection.getString("sound", "none") : "null") + "'");
+				}
+				else
+				{
+					openSound = new GUISound(sound, (float) soundSection.getDouble("volume", 10F), (float) soundSection.getDouble("pitch", 1F));
+				}
+			}
+			
+			if(configurationSection.contains("filler"))
+			{
+				fillerItem = ItemContainerConstructor.buildItemContainer(configurationSection.getConfigurationSection("filler")).toItemStack();
+			}
+			
+			guiElements = loadGUIElements(configurationSection, inventorySize.getSize());
+			
+			guiVariables = new ArrayDeque<>();
+			guiVariables.addAll(GUIVariables.getInstance().getAllRegisteredVariables());
+		}
 		
 		try
 		{
@@ -163,6 +173,8 @@ public class GUIBuilder<T>
 			                                                           String.class, String.class,
 			                                                           GUISound.class, ItemStack.class, Deque.class, Deque.class,
 			                                                           ConfigurationSection.class);
+			
+			constructor.setAccessible(true);
 			
 			return constructor.newInstance(guiTitle, inventorySize,
 			                               requiredPermission, noPermissionMessage,
