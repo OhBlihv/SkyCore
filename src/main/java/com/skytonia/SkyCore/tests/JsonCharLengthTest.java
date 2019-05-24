bd1d9be7d1952651d6c9482ff452d97e646b2cf7
OhBlihv <dev@ohblihv.me>
2019-05-24 14:50:39 +0800

    Work on JSON parsing line splitting (incomplete)

--- src/main/java/com/skytonia/SkyCore/tests/JsonCharLengthTest.java
+++ src/main/java/com/skytonia/SkyCore/tests/JsonCharLengthTest.java
@@ -1,7 +1,9 @@
 package com.skytonia.SkyCore.tests;
 
 import com.skytonia.SkyCore.util.BUtil;
+import com.skytonia.SkyCore.util.JSONContentParser;
 import com.skytonia.SkyCore.util.TextUtil;
+import net.md_5.bungee.api.chat.BaseComponent;
 import org.junit.Test;
 
 public class JsonCharLengthTest
@@ -13,11 +15,19 @@ public class JsonCharLengthTest
 		final int lineSplit = 60; //60 Chars?
 		final String lineLimit = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22";
 
-		String text = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34";
+		//String text = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34";
+		final String text = "&e&l(!) &e&lUNSCRAMBLE: &e{hov;Hover to see the word!;{scrambled-word}} &7Be the first to unscramble this word!";
 		final int textLength = TextUtil.getLineLength(text);
 
 		BUtil.log("Split Length = " + TextUtil.getLineLength(lineLimit));
 		BUtil.log("Line Length = " + textLength);
+
+		BUtil.log("--------------------");
+
+		for(BaseComponent resultingLine : new JSONContentParser(text).parseLines())
+		{
+			BUtil.log(">" + resultingLine.toLegacyText());
+		}
 	}
 
 }
--- src/main/java/com/skytonia/SkyCore/util/JSONContentParser.java
+++ src/main/java/com/skytonia/SkyCore/util/JSONContentParser.java
@@ -176,6 +176,11 @@ public class JSONContentParser extends VariableReplacer
 		}
 	}
 
+	private static final Pattern PATTERN_JSON_EVENT_CONTENT = Pattern.compile("\\{.*;(.*);.*}"),
+								 PATTERN_COLOUR = Pattern.compile("[&|§][a-r0-9]"); //Ignore bold for now
+	private static final String VARIABLE_JSON_CONTENT = "{JSON}";
+	private static final int MAX_LINE_LENGTH = 200; //140 or 240?
+
 	public BaseComponent[] parseLines()
 	{
 		formatMap.put(FormatType.COLOUR, "§f");
@@ -183,12 +188,126 @@ public class JSONContentParser extends VariableReplacer
 		Deque<BaseComponent> components = new ArrayDeque<>();
 		for(Iterator<String> contentItr = content.iterator();contentItr.hasNext();)
 		{
-			String line = contentItr.next();
+			final String originalLine = contentItr.next();
+			/*Deque<StringBuilder> visibleLines = new ArrayDeque<>();
+			Deque<StringBuilder> contentLines = new ArrayDeque<>();
 
-			parseLine(components, line, contentItr.hasNext());
+			visibleLines.addLast(new StringBuilder()); //Always edit the last line content
+			contentLines.addLast(new StringBuilder()); //Always edit the last line content
+			int bracketLevels = 0;
+			StringBuilder bracketContent = new StringBuilder();
+
+			int lineLength = 0;
+
+			for(String word : originalLine.split("((?<= )|(?= ))"))
+			{
+				final int openBracketCount = StringUtils.countMatches(word, "{"),
+					      closeBracketCount = StringUtils.countMatches(word, "}");
+
+				if(openBracketCount > 0 || closeBracketCount > 0)
+				{
+					//Add a marker where this content would go in the line
+					if(bracketLevels == 0 && openBracketCount > 0)
+					{
+						visibleLines.getLast().append("{JSON}");
+						contentLines.getLast().append("{JSON}");
+					}
+
+					bracketLevels += openBracketCount;
+					bracketContent.append(word);
+					bracketLevels -= closeBracketCount;
+
+					if(bracketLevels <= 0)
+					{
+						//Prevent negative bracket counts
+						bracketLevels = 0;
+
+						BUtil.log("Bracket content '" + bracketContent.toString() + "'");
+
+						//Attempt to measure/input the visible content of the line
+						final String jsonActualContent = bracketContent.toString();
+						String jsonVisibleContent = jsonActualContent;
+						Matcher jsonVisibleContentMatcher = PATTERN_JSON_EVENT_CONTENT.matcher(bracketContent.toString());
+						if(jsonVisibleContentMatcher.find())
+						{
+							jsonVisibleContent = jsonVisibleContentMatcher.group(1);
+						}
+
+						final int jsonIndex = visibleLines.getLast().indexOf(VARIABLE_JSON_CONTENT);
+						final int wordLength = TextUtil.getLineLength(PATTERN_COLOUR.matcher(jsonVisibleContent).replaceAll(""));
+						//Not enough space to replace. Remove our variable and start a new line
+						if(lineLength + wordLength > MAX_LINE_LENGTH)
+						{
+							//Remove the variable
+							visibleLines.getLast().replace(jsonIndex, jsonIndex + VARIABLE_JSON_CONTENT.length(), "");
+							//Line is too long, appendWord will create a new line correctly.
+							appendWord(jsonVisibleContent, lineLength, visibleLines);
+							//Add the entire JSON content to the actual line
+							appendWord(bracketContent.toString(), Integer.MAX_VALUE, contentLines);
+						}
+						//There's enough space for this replacement to occur
+						else
+						{
+							visibleLines.getLast().replace(jsonIndex, jsonIndex + VARIABLE_JSON_CONTENT.length(), jsonVisibleContent);
+							contentLines.getLast().replace(jsonIndex, jsonIndex + VARIABLE_JSON_CONTENT.length(), jsonActualContent);
+						}
+
+						bracketContent = new StringBuilder();
+					}
+				}
+				else
+				{
+					if(bracketLevels > 0)
+					{
+						bracketContent.append(word);
+					}
+					else
+					{
+						//Add to both the visible lines (to count length)
+						//and the actual content lines to be passed to the JSON parser
+						lineLength = appendWord(word, lineLength, visibleLines);
+						appendWord(word, Integer.MAX_VALUE, contentLines);
+					}
+				}
+			}
+
+			BUtil.log("Line: '" + visibleLines.getLast() + "");
+
+			if(bracketLevels != 0)
+			{
+				BUtil.log("Line has a non equal amount of bracket pairs! -> " + originalLine);
+			}
+
+			for(StringBuilder lineBuilder : contentLines)
+			{
+				parseLine(components, lineBuilder.toString(), contentItr.hasNext());
+			}*/
+
+			parseLine(components, originalLine, contentItr.hasNext());
 		}
 
 		return components.toArray(new BaseComponent[]{});
+	}
+
+	private int appendWord(String word, int lineLength, Deque<StringBuilder> splitLines)
+	{
+		final StringBuilder lineContent = splitLines.getLast();
+
+		//Strip colour codes when checking length
+		final int wordLength = TextUtil.getLineLength(PATTERN_COLOUR.matcher(word).replaceAll(""));
+		if(lineLength + wordLength > MAX_LINE_LENGTH)
+		{
+			BUtil.log("Line longer than " + MAX_LINE_LENGTH + " (" + lineLength + wordLength + ") -> '" + lineContent + "'");
+			BUtil.log("Splitting...");
+
+			BUtil.log("Line: '" + lineContent + "");
+
+			splitLines.addLast(new StringBuilder());
+			lineLength = 0;
+		}
+
+		lineContent.append(word);
+		return lineLength + wordLength;
 	}
 
 	private void parseLine(Deque<BaseComponent> components, String line, boolean hasNext)
